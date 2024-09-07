18caef8ee901cf885da43dead292e0751818dd80
Manuel López <manuellp87@gmail.com>
2024-09-07 10:56:40 +0200

    Added database configuration (#1)
    Added basic database configuration and structure.

---
+++ src/main/java/com/manu/inditex/capitole_test/price/domain/entity/Price.java
@@ -0,0 +1,47 @@
+package com.manu.inditex.capitole_test.price.domain.entity;
+
+import com.manu.inditex.capitole_test.brand.domain.entity.Brand;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="PRICES")
+@Getter
+@Setter
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "PRICE_ID", nullable = false, unique = true)
+    private Long priceId;
+
+    @ManyToOne
+    @JoinColumn(name = "BRAND_ID", nullable = false)
+    private Brand brandId;
+
+    @Column(name = "START_DATE", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DATE", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "PRICE_LIST", nullable = false)
+    private Integer priceList;
+
+    @Column(name = "PRODUCT_ID", nullable = false)
+    private Long productId;
+
+    @Column(name = "PRIORITY", nullable = false)
+    private Integer priority;
+
+    @Column(name = "PRICE", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "CURR", nullable = false, length = 3)
+    private String currency;
+}
